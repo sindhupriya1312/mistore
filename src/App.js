1260dc5d20e2e4bb967987694a50b2ec94ede56e
Divya Kumari <kumaridivya2796@gmail.com>
2023-07-23 00:51:57 +0530

    added offers section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-
-import './App.css';
-import PreNavbar from './components/PreNavbar';
-import Navbar from './components/Navbar.js'
+import "./App.css";
+import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
-import Slider from './components/Slider.js'
-import data from './data/data.json'
-
-
+import Slider from "./components/Slider.js";
+import data from "./data/data.json";
+import Offers from "./components/Offers.js";
 
 function App() {
-  return (   
-  <Router>
-    <PreNavbar />
-    <Navbar />
-  
-    <Slider start={data.banner.start} />
-  </Router>
+  return (
+    <Router>
+      <PreNavbar />
+      <Navbar />
+
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+    </Router>
   );
 }
 
